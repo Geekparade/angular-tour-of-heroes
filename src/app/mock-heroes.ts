7b2343124ad6e346a8ba4f4386476e76a213be13
Your Name <you@example.com>
2018-05-29 18:27:03 +0200

    projet en cours

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+	{ id: 1, name: 'Geralt de Riv' },
+	{ id: 2, name: 'Triss Merigold' },
+	{ id: 3, name: 'Yennefer de Vengerberg' },
+	{ id: 4, name: 'Jaskier' },
+	{ id: 5, name: 'Zoltan Chivay' },
+	{ id: 6, name: 'Eredin' }
+];
